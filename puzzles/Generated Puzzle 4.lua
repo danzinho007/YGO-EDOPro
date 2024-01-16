67d4c97f382fd0b17f3bcb37de38304c70ec63a8
danzinho007 <sedutordanzinho@hotmail.com>
2024-01-16 16:06:59 -0200

    add

---
+++ puzzles/Generated Puzzle 4.lua
@@ -0,0 +1,11 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+--Partially rewritten by edo9300
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,5)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--GY (yours)
+Debug.AddCard(160001000,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
