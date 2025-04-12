c94938044cc16819762bd749ad69e17d90c2a3da
danzinho007 <55755085+danzinho007@users.noreply.github.com>
2025-04-12 17:52:32 -0400

    add

--- expansions/script/teste.lua
+++ expansions/script/teste.lua
@@ -62,3 +62,85 @@ function s.can_be_used_as_material(tp)
     end
     return nil
 end
+
+
+====================
+--Golden Gift of Greed
+--Armadilha Normal
+--Compre 2 cartas, sendo 2 do oponente
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(1-tp,2) end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+    local p2,d2 = 1-tp,2
+    if Duel.Draw(p2,d2,REASON_EFFECT) > 0 then
+        local g = Duel.GetOperatedGroup()
+        Duel.ConfirmCards(tp,g)
+        Duel.SendtoHand(g,tp,REASON_EFFECT)
+    end
+end
+
+=========================
+--Golden Pot of Greed
+--Magia Normal
+--Compra 4 cartas, sendo 2 do inimigo
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+--Verifica se pode comprar 4 cartas:
+	if chk==0 then 
+		return Duel.IsPlayerCanDraw(tp,2) and Duel.IsPlayerCanDraw(1-tp,2)
+	end
+--Quem vai comprar carta ?
+--tp = jogador e 1-p oponente
+	Duel.SetTargetPlayer(tp)
+--Quantas cartas serão compradas ?
+	Duel.SetTargetParam(2)
+--Informações auxiliares sobre a compra:
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+--O oponente "compra" 2 cartas, mas elas irão para a mão do jogador
+    local p2,d2 = 1-tp,2
+    if Duel.Draw(p2,d2,REASON_EFFECT) > 0 then
+--Obtem as cartas compradas pelo oponente
+        local g = Duel.GetOperatedGroup()
+--Revela as cartas ao jogador
+        Duel.ConfirmCards(tp,g)
+-- Move as cartas para a mão do jogador
+        Duel.SendtoHand(g,tp,REASON_EFFECT) 
+    end
+end
+--30 ate 43 = 13, sendo 4 comentarios
+--entao 13 - 4 = 9
